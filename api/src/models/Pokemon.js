a86230a74de04bc38f1b91720c0b165ea32491b0
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-03-26 13:47:56 -0300

    First Commit

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    hp: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    attack: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    defense: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    speed: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    height: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    weight: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   });
 };
