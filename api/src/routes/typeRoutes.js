40c9e608d50c0ff504f99d326490c6baf03791f2
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-03-28 21:28:43 -0300

    typeRoutes
    get /types funciona

--- api/src/routes/typeRoutes.js
+++ api/src/routes/typeRoutes.js
@@ -1,14 +1,21 @@
 const { Router } = require('express');
-const { Type } = require('../db')
+const { Type } = require('../db');
 const router = Router();
+const axios = require('axios');
 
-router.get('/types', async (req, res, next)  => {
-    try {
-        const type = await Type.findAll()
-        res.send(type);
-    } catch (error) {
-        next(error);
-    }
+router.get('/types', async (req, res, next)  => { // FUNCIONA
+    const typesApi = await axios.get('https://pokeapi.co/api/v2/type')
+    const typesArray = await typesApi.data.results;
+    
+    typesArray.forEach(type => {
+        Type.findOrCreate({
+            where: {
+                name: type.name
+            }
+        })
+    });
+    const allTypes = await Type.findAll();
+    res.send(allTypes);
 })
 
 
