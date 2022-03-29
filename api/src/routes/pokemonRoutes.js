4bf77f6e9c6202707f512650530f9937d7bfb134
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-03-29 20:21:53 -0300

    mejora del back

--- api/src/routes/pokemonRoutes.js
+++ api/src/routes/pokemonRoutes.js
@@ -8,42 +8,40 @@ router.get('/pokemons', async(req, res, next)  => { // FUNCIONA -> Ver por que s
     // Aca combinamos la información de la api y la info de mi base de datos en una sola request.
     // Para obtener los personajes del link que nos dan de pokemons, vamos a usar Axios:
     // Es una libreria que funciona con fetch.
-    const {name} = req.query; 
-    const totalPokemons = await getAllInfo();
-    if (name){
-        const pokemonName = totalPokemons.filter(ele=>ele.name.toLowerCase().includes(name.toLowerCase()))
-        if (pokemonName.length) {
-            return res.status(200).send(pokemonName);
-        } return res.send({error: 'Pokemon not found'})
-    } else {
+    // El query se pasa por URL y el body se pasa para coompletar un post con info. (ej formulario)
+    const { name } = req.query;  // Funciona con name /pokemons?name=pikachu
+    const totalPokemons = await getAllInfo(); // Funciona con todos los pokemons + DB
+    if (name) {
+        const pokemonName = totalPokemons.filter(el => el.name.toLowerCase().includes(name.toLowerCase()));
+        pokemonName.length ? res.status(200).send(pokemonName) : res.status(404).send({error: 'Pokemon not found'})
+    } else { // Si no hay un query name:
         try {
-            return res.status(200).send(totalPokemons);
-        } catch(error){
+            res.status(200).send(totalPokemons);
+        } catch(error) { 
             res.send(error)
         }
     }
 })
 
-router.post('/pokemons', async (req, res)=>{
-    const {
-       img, name, types, hp, attack, defense, speed, weight, height } = req.body;
+router.post('/pokemons', async (req, res) => {
+    const { img, name, types, hp, attack, defense, speed, weight, height } = req.body;
     try {
-        let newPokemon = await Pokemon.create({
-            img, name, hp, attack, defense, speed, weight, height
-        });
+        let newPokemon = await Pokemon.create({ // NO le paso types porque tengo que hacer relación aparte.
+            img, name, hp, attack, defense, speed, weight, height 
+        }); 
 
-        let typeDb = await Type.findAll({
-            where: { name: types }
+        let typeDb = await Type.findAll({ // el Type lo tengo que encontrar en el modelo Type que tiene todas las ocupaciones.-
+            where: { name: types }        // name que coincida al types que le llega por body.
         });
-
-        newPokemon.addType(typeDb);
-        res.send(newPokemon);
+        newPokemon.addType(typeDb); // addType es un método de Sequelize que me trae de la tabla Type el typeDb. 
+                                    // A el nuevo Pokemon, agregale el type que le pase por body.
+        res.send("Pokemon created successfully!");
     } catch (error){
         res.send(error);
-    }
+    } 
 })
 
-router.get('/pokemons/:idPokemon', async(req, res, next)  => { // FUNCIONA con id y con name /pokemons?name=pikachu
+router.get('/pokemons/:idPokemon', async(req, res, next)  => { // FUNCIONA con id 
     const { idPokemon } = req.params;
     const totalPokemons = await getAllInfo();
     if (idPokemon) {
--- api/src/routes/typeRoutes.js
+++ api/src/routes/typeRoutes.js
@@ -3,20 +3,19 @@ const { Type } = require('../db');
 const router = Router();
 const axios = require('axios');
 
-router.get('/types', async (req, res, next)  => { // FUNCIONA
-    const typesApi = await axios.get('https://pokeapi.co/api/v2/type')
-    const typesArray = await typesApi.data.results;
-    
-    typesArray.forEach(type => {
-        Type.findOrCreate({
-            where: {
-                name: type.name
-            }
+router.get('/types', async (req, res)  => { // FUNCIONA
+    const typesApi = await axios.get('https://pokeapi.co/api/v2/type') // Entra a la API
+    const typesArray = await typesApi.data.results; // Agarra la info dentro del array results.
+    typesArray.forEach(types => { // Entra al modelo y le dice encontrala o creala si no la tenes a cada type
+        Type.findOrCreate({       // En donde el nombre sea ese elemento q me pasan
+            where: { name: types.name }
         })
     });
-    const allTypes = await Type.findAll();
+    const allTypes = await Type.findAll(); // una vez hecho todo, entrá a Type, traeme todo y mandamelo
     res.send(allTypes);
 })
-
+// Si tengo el force seteado en false, lo va a traer todo directamente. 
+// Si está en true, lo vuelve a hacer cada vez que levanto el back.-
+// NO hace falta usar try catch con async await.-
 
 module.exports = router;
