630cf7f0d7909e0841999f4269639b8a0dd388fc
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-02 01:00:54 -0300

    First css commit XD

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './Card.module.css'
 
 function Card( {name, img, types }) {
   return (
-    <div>
+    <div className={styles.card}>
         <h3>{name}</h3>
         <h5>{types}</h5>
         <img src={img} alt="img not found" width="200px" height="200px" />
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,6 +10,7 @@ import { getPokemons, filterPokemonsByTypes, filterPokemonsCreated, orderByName,
 import Card from '../Card/Card'
 import Pagination from '../Pagination/Pagination';
 import SearchBar from '../SearchBar/SearchBar';
+import styles from './Home.module.css'
 
 
 // Siempre que voy a trabajar con algo que funcione solo en este componente (HOME), lo establezco acá. Para no perder tiempo haciendo toda esta lógica en un estado global, genero estado local en este componente. 
@@ -100,20 +101,22 @@ function Home() {
                 />
                 
                 <SearchBar />
-                
-                {
-                load ? (<h1>Cargando bro</h1>) :
-                    !allPokemons.length? (<h1>No se encontro nada capo</h1>) :
-                    currentPokemons?.map((el) => {
-                        return (
-                            <div>
-                                <Link to={'/home/' + el.id}>
-                                    <Card key={el.id} name={el.name} img={el.img} types={el.types}/>
-                                </Link>
-                            </div>
-                        );
-                    })
-                }
+                <div className={styles.cardsContainer}>
+                    {
+                    load ? (<h1>Cargando bro</h1>) :
+                        !allPokemons.length? (<h1>No se encontro nada capo</h1>) :
+                        currentPokemons?.map((el) => {
+                            return (
+                                <div>
+                                    <Link to={'/home/' + el.id}>
+                                        <Card key={el.id} name={el.name} img={el.img} types={el.types} />
+                                    </Link>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+
             </div>
         </div>
     )
