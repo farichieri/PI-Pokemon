10d7c55b840b029499aec6ffb97f45ac82ab9730
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-04 11:52:55 -0300

    CSS

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,9 +4,11 @@ import styles from './Card.module.css'
 function Card( { name, img, types }) {
   return (
     <div className={styles.card}>
-        <h3>{name}</h3>
-        <h5>{types}</h5>
-        <img src={img} alt="img not found" width="200px" height="200px" />
+      <div className={styles.cardContainer}>
+        <h3 className={styles.cardName}>{name}</h3>
+        <h5 className={styles.cardTypes}>{types}</h5>
+        <img className={styles.cardImg} src={img} alt="img not found" width="200px" height="200px" />
+      </div>
     </div>
   )
 }
