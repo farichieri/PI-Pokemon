9ce12beda550963f235c0a6fa41ee29fab19b22d
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-16 21:04:28 -0300

    Mejora

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -8,6 +8,7 @@ export default function LandingPage() {
             <div className={styles.welcomeText}>
                 <h1>Welcome</h1>
             </div>
+            <h1 className={styles.casePhone}>I'm sorry, I'm not responsive at the moment :(</h1>
             <div className={styles.welcome}>
                 <div className={styles.image} />
                 <Link to='/home'> 
